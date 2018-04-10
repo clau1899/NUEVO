929b4b2becc61adb97542e78f2ace5a9ca649a63
clau1899 <30761140+clau1899@users.noreply.github.com>
2018-04-10 10:42:43 -0500

    Update main.cpp
    funcion y op. ternario

--- Problema3/main.cpp
+++ Problema3/main.cpp
@@ -3,19 +3,25 @@
 using namespace std;
 
 //Implemente un programa que solicite el ingreso de 3 numeros e imprima: (i)
-//el menor, (ii) el mayor y (iii) el promedio de dichos numeros.
-
+//el menor, (ii) el mayor b (iii) el promedio de dichos numeros.
+void mayMenProm(int a, int b, int c, int &mayor, int &menor, float &prom){
+    (a<b && a<c)? menor=a:(b<a && b<c)?menor=b:menor=c;
+    (a>b && a>c)? mayor=a:(b>a && b>c)?mayor=b:mayor=c;
+    prom=(a+b+c)/3;
+}
 int main()
 {
-    float x,y,z;
+    int a,b,c,mayor,menor;
+    float prom;
     cout << "Ingrese el primer numero" << endl;
-    cin>>x;
+    cin>>a;
     cout << "Ingrese el segundo numero" << endl;
-    cin>>y;
+    cin>>b;
     cout << "Ingrese el tercer numero" << endl;
-    cin>>z;
-    (x<y && x<z)?cout<<"El menor es: "<<x<<"\n":(y<x && y<z)?cout<<"El menor es: "<<y<<"\n":cout<<"El menor es: "<<z<<"\n";
-    (x>y && x>z)?cout<<"El mayor es: "<<x<<"\n":(y>x && y>z)?cout<<"El mayor es: "<<y<<"\n":cout<<"El mayor es: "<<z<<"\n";
-    cout<<"El promedio de los numeros es: "<<(x+y+z)/3<<endl;
+    cin>>c;
+    mayMenProm(a,b,c,mayor,menor,prom);
+    cout<<"el menor es: "<<menor<<endl;
+    cout<<"el mayor es: "<<mayor<<endl;
+    cout<<"el promedio es: "<<prom<<endl;
     return 0;
 }
