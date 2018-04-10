c443150c421e176a76bbd3904c9bd0387276566b
clau1899 <30761140+clau1899@users.noreply.github.com>
2018-04-10 10:56:53 -0500

    Update main.cpp

--- Problema2/main.cpp
+++ Problema2/main.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 //Implementar un programa que solicite un numero n e imprima los numero desde el 1 hasta n separados por comas.
 //OJO, despues del  ́ultimo n ́umero no debe imprimir la ’,’.
-
-int main()
+void hastaN (int x)
 {
-    int x;
-    cout << "Ingrese un numero por favor" << endl;
-    cin>>x;
     for (int i=1;i<=x;i++){
         cout<<i;
         if (i<x){
             cout<<", ";
         }
     }
+}
+int main()
+{
+    int x;
+    cout << "Ingrese un numero por favor" << endl;
+    cin>>x;
+    hastaN(x);19
     return 0;
 }
