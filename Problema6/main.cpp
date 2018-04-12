cd00ff3978fe004211ab224ccf64c279beb30849
clau1899 <30761140+clau1899@users.noreply.github.com>
2018-04-11 23:12:01 -0500

    Update main.cpp

--- Problema6/main.cpp
+++ Problema6/main.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 //Implemente un programa que solicite un numero n e imprima todos los numeros primos menores a n.
 
+int primosMenoresaN(int n,int numero=2,int cont=2){
+    if (numero==n)
+        return 0;
+    if(cont==numero){
+        cout<<numero<<endl;
+        return primosMenoresaN(n,numero+=1,2);
+    }
+    if (numero%cont==0)
+        return primosMenoresaN(n,numero+=1,2);
+    return primosMenoresaN(n,numero,cont+=1);
+}
 int main()
 {
     int n;
     cout<<"Ingrese el numero por favor"<<endl;
     cin>>n;
-    for (int j=2;j<=n;j++){//por definicion el 1 no es primo, se empieza en 2
-        int a=0;//es importante declarar el contador dentro de
-        for(int i=1;i<=n;i++){//divide a j entre los números del 1 a n
-            if(j%i==0) 
-                a++;
-        }
-
-        if (a<=2) //si solo tiene dos números divisores entonces es primo y se imprime
-        cout<<j<<endl;
-    }
+    primosMenoresaN(n);
     return 0;
 }
+
