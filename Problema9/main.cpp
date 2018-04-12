8466cc7c59aacc7eea3c29907a09d72f670c4405
clau1899 <30761140+clau1899@users.noreply.github.com>
2018-04-11 23:45:01 -0500

    Update main.cpp

--- Problema9/main.cpp
+++ Problema9/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 //Implemente un programa que evalue si un a ̃no es bisiesto.
 //bisiesto= si es divisible entre 4, excepto aquellos divisibles entre 100 pero no entre 400.
 
-int main()
-{
-    int x;
-    cout << "Ingrese el año: " << endl;
-    cin>>x;
-    if (x%4==0 && (x%100!=0 || x%400==0))
-        cout<<x<< " es un año bisiesto"<<endl;
-    else
-        cout<<x<<" no es un año bisiesto"<<endl;
+bool bisiesto(int numero){
+    return(!(numero%4))?((!(numero%100)&&(numero%400))?false:true):false;
+}
+int main( ){
+
+    int n;
+    cout << "Introduce un numero entero: \n";
+    cin >> n;
+    bisiesto(n);
     return 0;
 }
