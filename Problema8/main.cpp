6999b47816c48ea9842cb279d73d84f118373ebd
clau1899 <30761140+clau1899@users.noreply.github.com>
2018-04-11 23:40:04 -0500

    Update main.cpp

--- Problema8/main.cpp
+++ Problema8/main.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 //Implemente un programa que solicite un numero de cinco digitos e indique si se trata de un palindrome.
 
-int main()
-{
-    int x;
-    cout << "Ingrese un numero de 5 digitos: " << endl;
-    cin>>x;
-    if (x/10000 == (((x%10000)%1000)%100)%10 && (x%10000)/1000 == (((x%10000)%1000)%100)/10 )
-        cout<<x<<" es un palindrome"<<endl;
-    else
-        cout<<x<<" no es un palindrome"<<endl;
+int esPalindrome(int n, int ndig=5){
+    if (n<10) return n;
+    else return n%10*ndig + esPalindrome(n/10, ndig/10);
+}
+int main( ){
+
+    int n,ndig=1,aux;
+    cout << "Introduce un numero entero: \n";
+    cin >> n;
+    aux=n;
+    while (aux>=10){
+      aux/=10;
+      ndig*=10;
+    }
+    aux=esPalindrome(n,ndig);
+    (n==aux)?cout<<"Es capicua":cout<<"No es capicua";
     return 0;
 }
