73761b641ed72cbb2475b264ea0f2436e41d4bb0
clau1899 <30761140+clau1899@users.noreply.github.com>
2018-04-11 23:01:18 -0500

    Update main.cpp

--- Problema5/main.cpp
+++ Problema5/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 //Implemente un programa que solicite un numero y evalue si dicho numero es primo
 
-bool primoRecursivo(int numero, int c)
+bool primoRecursivo(int numero, int c=2)
 {
-    if (numero %c==0 && numero !=2)
+    if (numero%c==0 && numero !=2)
         return false;
     else if (c>numero/2)
         return true;
@@ -16,12 +16,9 @@ bool primoRecursivo(int numero, int c)
 
 int main()
 {
-    int num;
-    cout<<"Ingrese el numero por favor; "<<endl;
-    cin>>num;
-    if(primoRecursivo(num,2))//si la funcion delvuelve true 1
-        cout<<"Es Primo";
-    else
-        cout<<"No es primo";
+    int x;
+    cout << "Ingrese un numero" << endl;
+    cin>>x;
+    primoRecursivo(x)?cout<<"Es Primo":cout<<"No es primo";
     return 0;
 }
