5459ca12bd5f01f0adc53062fa933aad70e1a697
clau1899 <30761140+clau1899@users.noreply.github.com>
2018-04-11 23:53:31 -0500

    Update main.cpp

--- Problema10/main.cpp
+++ Problema10/main.cpp
@@ -4,16 +4,19 @@ using namespace std;
 
 //Implemente un programa que solicite un numero n e imprima los n numeros de la serie fibonacci.
 
-int main()
+int fibo(int numero)
 {
-    int n,a=0,b=1,c;
-    cout << "Ingrese un numero: " << endl;
-    cin>>n;
-        for (int i=1;i<=n;i++){
-            c=a+b;
-            cout<<c<<"  ";
-            a=b;
-            b=c;
-    }
+    if((numero==0)||(numero==1))
+        return 1;
+    return fibo(numero-1)+fibo(numero-2);
+}
+int main( ){
+
+    int n;
+    cout << "Introduce un numero entero: \n";
+    cin >> n;
+    //fibo(n);
+    for (int i=0;i<n;i++)
+        cout<<fibo(i)<<", ";
     return 0;
 }
